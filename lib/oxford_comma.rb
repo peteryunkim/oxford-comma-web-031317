a46435041b1242646ebbec1cf1d414675959bd52
Peter Kim <peteryunkim@gmail.com>
2017-03-11 02:31:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(" and ")
+  else array.length > 2
+    last_word = array.pop
+    new_word = "and #{last_word}"
+    array << new_word
+    array.join(", ")
+  end
 end
